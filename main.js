00882b8a280914c131745fe794e0d4552975484a
Kuantize Strigoi <adrian.sunye@gmail.com>
2023-09-19 18:10:35 +0200

    Added aliases to package json to make routes easier to access

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 import './style.css'
-import $ from "./src/modules/jquery_module.js"
-import header_navigation from './src/components/navigation/header_navigation/header_navigation'
-import { setupHoverMenu } from './src/modules/hover_menu/hover_menu';
-import { setupNavigationItems } from './src/modules/navigation_items/navigation_items';
+import $ from "#modules/jquery_module.js"
+import header_navigation from '#components/navigation/header_navigation/header_navigation'
+import product_section from '#components/layout/sections/product_section/product_section'
+import { setupHoverMenu } from '#modules/hover_menu/hover_menu';
+import { setupNavigationItems } from '#modules/navigation_items/navigation_items';
     
 $(document).ready(function(){  
   $('#header_navigation').append(
     header_navigation,
   );
-  
+
   $("nav .hover-menu").each(function() {
     setupNavigationItems($( this ))
     setupHoverMenu($( this ))
   })
+
+  $('#product_section').append(
+    product_section
+  );
 });
 
 
---
+++ src/components/layout/sections/product_section/product_section.js
@@ -0,0 +1,24 @@
+import product from '#assets/images/product.png'
+export default /*html*/ `
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-8">
+			<div class="row">
+				<div class="col-md-6">
+				</div>
+				<div class="col-md-6">
+                    <img src="${product}">
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="row">
+				<div class="col-md-6">
+				</div>
+				<div class="col-md-6">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+`
