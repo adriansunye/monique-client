fcf267187aa30769a463aacfda45837974297246
Kuantize Strigoi <adrian.sunye@gmail.com>
2023-09-20 19:42:46 +0200

    Created modal component and functionality to close video reproduction on hide

--- src/components/modals/demostration_modal/demostration_modal.js
+++ src/components/modals/demostration_modal/demostration_modal.js
@@ -1,13 +1,15 @@
+import './demostration_modal.css'
+
 const DEMOSTRATION_URL = "https://www.youtube-nocookie.com/embed/hCWIMxAzlrM?si=0Hg0UEyyT-Nb9Dfc"
 
 export default /*html*/ `
     <!-- Modal -->
-    <div class="modal fade" id="demostrationModal" data-bs-backdrop="true" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal fade modal-fullscreen" id="demostrationModal" data-bs-backdrop="true" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" data-bs-dismiss="modal">
             <div class="modal-content">
                 <div class="modal-body">
                     <div class="embed-responsive embed-responsive-16by9">
-                        <iframe id="demostrationVideo" type="text/html" width="854" height="480" src="${DEMOSTRATION_URL}" 
+                        <iframe id="demostrationVideo" type="text/html"  src="${DEMOSTRATION_URL}" 
                             title="YouTube video player" frameborder="0">
                         </iframe>                   
                     </div>
