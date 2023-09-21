a21aa397cc5ab0901733c408fd9448b285ea3311
Kuantize Strigoi <adrian.sunye@gmail.com>
2023-09-21 10:33:23 +0200

    Edit information_section with a card grid and appended cards depending on information received from JSON on information_cards module

---
+++ src/modules/information_section/information_cards.js
@@ -0,0 +1,19 @@
+import $ from "#modules/jquery_module.js"
+
+export function setupInformationCards(info) {
+    const setInformationCards = () => {
+        info.items.forEach((el) => {
+            $("#information_section .cards").append(/*html*/`
+            <div class="col-md-8-auto">
+                <div class="card h-100 text-dark bg-light text-center">
+                    <div class="card-body">
+                        <h5 class="card-title">${el.title}</h5>
+                        <p class="card-text">${el.text}</p>
+                    </div>
+                </div>
+            </div>`
+            )
+        });
+    }
+    setInformationCards();
+}
